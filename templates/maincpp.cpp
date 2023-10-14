0e603b36a67bdb9562b0dd61eef7c6b150222819
longhai-mine <longhaitechnis@gmail.com>
2023-10-14 10:10:01 +0700

    Van minh

---
+++ templates/maincpp.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ii pair < int , int >
+#define iii pair < int , ii >
+#define iv pair < ii , ii >
+
+#define fi first
+#define se second
+#define fr front()
+#define pb push_back
+
+#define FOR(i , x , n) for(int i = x ; i <= n ; ++i)
+#define REP(i , n) for(int i = 0 ; i < n ; ++i)
+#define FORD(i , x , n) for(int i = x ; i >= n ; --i)
+
+int main()
+{
+    //freopen("gen.inp", "r", stdin);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+}
